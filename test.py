8c04281c1d6b1d0c145a839e8a989cbf18bfabdf
Anderson Thomas <anderson@diva.cards>
2023-01-19 21:21:54 +1030

    update

--- test.py
+++ test.py
@@ -6,25 +6,28 @@ ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 
 opsc = OPSClient(BASE_URL, ACCESS_TOKEN)
 
-# # Instance Types
+# Instance Types
+print("listing available instance types...")
 instance_types = opsc.list_instance_types(
     [
         lambda i: len(i.regions) > 0,
         lambda i: i.instance_type.price_cents_per_hour < 100,
     ]
 )
+print("listing running instances...")
 instances = opsc.list_instances()
-#
-# # With SSH Keys
-# ssh_key = opsc.create_ssh_key()
+
+# With SSH Keys
+print("creating ssh key...")
+ssh_key = opsc.create_ssh_key()
+print("listing available ssh keys...")
 ssh_keys = opsc.list_ssh_keys()
-#
-#
-# # Print the results
+
+# Print the results
 print(instance_types)
 print(instances)
 #
-# print(ssh_key)
+print(ssh_key)
 print(ssh_keys)
 
 # instance = opsc.create_instance(region_name="hello")
