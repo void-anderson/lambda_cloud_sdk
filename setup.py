2eaf816eed7e5aeacf399313240afd23d15cf8ca
Anderson Thomas <anderson@diva.cards>
2023-02-28 22:04:30 +1030

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LamdaCloudSDK",
+    version="0.0.1",
+    url="https://github.com/void-anderson/lambda_cloud_sdk",
+    author="Thomas Anderson",
+    author_email="anderson@diva.cards",
+    description="SDK for Lamda GPU Cloud",
+    packages=find_packages(),
+    install_requires=[],
+)
